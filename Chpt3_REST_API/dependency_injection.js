4e94fcbfd323cc6be53378e13196e1724b2b9e03
Lucas <lufan1988@gmail.com>
2016-11-11 20:56:29 -0600

    Chapter 3 dependency injection

---
+++ Chpt3_REST_API/dependency_injection.js
@@ -0,0 +1,42 @@
+/**
+ *	Writing dependency injection using npm wagner module.
+ */
+var express = require('express');
+var mongoose = require('mongoose');
+var wagner = require('wagner-core');
+var util = require('util');
+
+setupModels(mongoose, wagner);
+
+var app = express();
+
+setupApp(app, wagner);
+
+app.listen(3000);
+console.log('Listening on port 3000!');
+
+function setupModels(mongoose, wagner) {
+	mongoose.connect('mongodb://localhost:27017/test');
+	var userSchema = new mongoose.Schema({
+		name: String
+	});
+
+	var User = mongoose.model('User', userSchema);
+
+	wagner.factory('User', function() {
+		return User;
+	});
+}
+
+function setupApp(app, wagner) {
+	var routeHandler = wagner.invoke(function(User) {
+		return function(req, res) {
+			//console.log(util.inspect(req, false, null))
+			User.find({ name: req.params.id}, function(error, user) {
+				res.json({ user: user });
+			});
+		}
+	});
+
+	app.get('/user/:id', routeHandler);
+}
