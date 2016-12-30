02b86314a0f0b9419407dbc4e2628466b72db7c6
Lucas <lufan1988@gmail.com>
2016-12-29 22:16:46 -0600

    Chpt4: Angular Services and HTTP Part1
    1. Adding Express middleware to serve static file in node.js
    2. Login through facebook in hello-http.html

--- Chpt3_Retail_Store_API/index.js
+++ Chpt3_Retail_Store_API/index.js
@@ -10,6 +10,14 @@ wagner.invoke(require('./auth'), { app: app });
 
 app.use('/api/v1', require('./api')(wagner));
 
+/**
+ * Using the Express middleware to serve static files
+ * first argument '/public' is to set up a virtual path
+ * The second argument Angular is the actual path that contain the static file
+ */
+
+app.use('/public', express.static('Angular'));
+
 app.listen(3000);
 
 console.log('Listening on port 3000!');
